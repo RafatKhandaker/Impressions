0bbbb695948bf7df3807ca05240db8c3c8e7aa84
Black Jack <Rafat.Khandaker@gmail.com>
2018-03-05 14:08:09 -0500

    added time-stamp to error logs

--- src/main/java/com/blackjack/Model/ErrorLog.java
+++ src/main/java/com/blackjack/Model/ErrorLog.java
@@ -1,22 +1,36 @@
 package com.blackjack.Model;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.validation.constraints.Null;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @SuppressWarnings("unused")
 @Document(collection = "ErrorLog")
 public class ErrorLog {
+	
+	@Id
+	private String id;
 	private String username;
+	@Null
 	private String password;
 	private String log;
+	private Timestamp timeStamp;
 	
 	public ErrorLog(String username, Exception error) {
 		this.username = username;
 		this.log = error.toString();
+		this.timeStamp = new Timestamp(System.currentTimeMillis());
 	}
 	
 	public ErrorLog(String username, String password, Exception error) {
 		this.username = username;
 		this.password = password;
 		this.log = error.toString();	
+		this.timeStamp = new Timestamp(System.currentTimeMillis());
+
 	}
 }
