9e9e0205b24992dd4a4ce8f41dd809bb37bc857c
Black Jack <Rafat.Khandaker@gmail.com>
2018-03-05 01:01:38 -0500

    updated bottom scripts and fixed login page.. removed all redundant tags

--- src/main/resources/static/scripts/pages/login-script.js
+++ src/main/resources/static/scripts/pages/login-script.js
@@ -1,9 +1,14 @@
 var modal = $('#id01');
 var close = $('.close');
 var cancelBtn = $('.cancelbtn');
+var loginBtn = $('#loginBtn');
 
 function closeLoginForm(){
 	this.modal.css('display', 'none');
+}
+
+function openLoginForm(){
+	this.modal.css('display', 'block')
 }
 
 window.onclick = function(e) {
@@ -15,9 +20,11 @@ window.onclick = function(e) {
 	if(e.target.className == this.cancelBtn.attr('class')){
 		closeLoginForm();
 	}
+	
+	if(e.target.id == this.loginBtn.attr('id')){
+		openLoginForm();
+	}
 }
 
-
-v
 
 		
