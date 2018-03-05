e19c347e4ab97623273c9c076deada06234c40c6
Black Jack <Rafat.Khandaker@gmail.com>
2018-03-05 00:44:33 -0500

    cleanup login script

--- src/main/resources/static/scripts/pages/login-script.js
+++ src/main/resources/static/scripts/pages/login-script.js
@@ -1,11 +1,23 @@
 var modal = $('#id01');
 var close = $('.close');
+var cancelBtn = $('.cancelbtn');
 
-window.onclick = function(e) {
-		if(e.target.className == this.close.attr('class')){
-			this.modal.css('display', 'none');
-		}
+function closeLoginForm(){
+	this.modal.css('display', 'none');
 }
 
+window.onclick = function(e) {
+		
+	if(e.target.className == this.close.attr('class')){
+		closeLoginForm();
+	}
+		
+	if(e.target.className == this.cancelBtn.attr('class')){
+		closeLoginForm();
+	}
+}
+
+
+v
 
 		
