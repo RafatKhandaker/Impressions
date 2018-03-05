0fabff5668f89f10ef297c3499a0c36fe24a9e0f
Black Jack <Rafat.Khandaker@gmail.com>
2018-03-05 00:35:30 -0500

    fixed javascript bug

--- src/main/resources/static/scripts/pages/login-script.js
+++ src/main/resources/static/scripts/pages/login-script.js
@@ -2,19 +2,10 @@ var modal = $('#id01');
 var close = $('.close');
 
 window.onclick = function(e) {
-	
-		if (e.target == this.modal) {
-			this.modal.css('display: none');
-		}
-		
-		if(e.target == this.close){
-			this.modal.css('display: none');
+		if(e.target.className == this.close.attr('class')){
+			this.modal.css('display', 'none');
 		}
 }
 
-function pageLoad(this.modal){
-	this.close.onclick = function(e){
-		modal.css('display: none')
-	}
-}
+
 		
