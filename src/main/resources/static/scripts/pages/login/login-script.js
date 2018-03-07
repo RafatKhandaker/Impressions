0334dfa919eae5f7ee36fc716a07b2f21a088cbc
Black Jack <Rafat.Khandaker@gmail.com>
2018-03-07 03:12:10 -0500

    added validate password check on login form and cleanup java-script for validation

--- src/main/resources/static/scripts/pages/login/login-script.js
+++ src/main/resources/static/scripts/pages/login/login-script.js
@@ -1,12 +1,13 @@
 (function() {
-	  var darkGrey, emailVerify, emerald, forget, lightGrey, login, passwordSecure, red, signup, submit, toForget, toLogin, toSignup, yellow, form;
+	  var darkGrey, emailVerify, emerald, forget, lightGrey, login, passwordSecure, red, signup, submit, toForget, toLogin, toSignup, yellow, form, validate;
 
 	  form = $(".form-class");
 	  login = $(".login");
 	  signup = $(".sign-up");
 	  forget = $(".forget");
 	  submit = $('.button');
-	 
+	  validate = $('.validator');
+
 	  emerald = "#19CC8B";
 	  red = "#BC3E48";
 	  yellow = "#B8B136";
@@ -18,7 +19,12 @@
 	    $(".security").addClass("hide");
 	    $(".full-name, .retype").addClass("ani-hide");
 	    $(".password, .password div").removeClass("ani-hide");
+	    
 	    login.addClass("selected");
+	    
+	    validate.removeClass("display");
+	    validate.addClass("hide");
+
 	    signup.removeClass("selected");
 	    emailVerify();
 	    forget.show();
@@ -30,7 +36,7 @@
 			  form.attr('action','/login/login');
 		  }
 		  if(signup.hasClass('selected')){
-			  form.attr('action', '/login/register')
+			  form.attr('action', '/login/register');
 		  }
 		  return form.submit();
 	  };
@@ -39,6 +45,10 @@
 	  toSignup = function() {
 	    passwordSecure();
 	    $(".full-name, .retype, .password").removeClass("ani-hide");
+	    
+	    validate.removeClass("display");
+	    validate.addClass("hide");
+
 	    signup.addClass("selected");
 	    login.removeClass("selected");
 	    emailVerify();
@@ -61,16 +71,20 @@
 	  
 	  emailVerify = function() {
 	    var checkInterval, myInterval;
+	    
 	    if ($(".login").hasClass("selected")) {
 	      checkInterval = 0;
+	     
 	      return myInterval = setInterval((function() {
 	        if ($(".email .content").val().length >= 18) {
-	          $(".profile-img").addClass("profile-pic");
-	          $(".profile-add").hide();
-	          clearInterval(myInterval);
+	          
+	         	$(".profile-img").addClass("profile-pic");
+	         	$(".profile-add").hide();
+	          
+	         	clearInterval(myInterval);
 	        }
 	        if (checkInterval === 250) {
-	          clearInterval(myInterval);
+	        		clearInterval(myInterval);
 	        }
 	        return checkInterval++;
 	      }), 250);
@@ -84,53 +98,60 @@
 	  passwordSecure = function() {
 	    var checkInterval, myInterval;
 	    checkInterval = 0;
-	    return myInterval = setInterval((function() {
-	      var backFill, color, pie1, pieColor, secureVal, value;
-	      value = $(".password .content").val();
-	      if (value.length > 0 && value.length < 4) {
-	        color = red;
-	        backFill = red;
-	      } else if (value.length >= 5 && value.length < 7) {
-	        color = yellow;
-	        backFill = yellow;
-	      } else if (value.length >= 8) {
-	        color = emerald;
-	        backFill = emerald;
-	      }
-	      if (value.length > 0) {
-	        $(".security").removeClass("hide");
-	      } else {
-	        $(".security").addClass("hide");
-	      }
-	      secureVal = value.length * 9;
-	      if (secureVal >= 100) {
-	        secureVal = 100;
-	      }
-	      if (value.length <= 5) {
-	        pie1 = (value.length * 36) + 90;
-	        pieColor = lightGrey;
-	      } else if (value.length >= 5 && value.length <= 9) {
-	        pieColor = color;
-	        pie1 = (value.length * 36) - 90;
-	      } else {
-	        secureVal = 90;
-	        pie1 = 270;
-	      }
-	      $(".secureValue").html(secureVal);
-	      console.log(pie1 + " " + secureVal);
-	      $(".password .content, .password .security-type").css("color", `${color}`);
-	      $(".circle.background").css("background", `${backFill}`);
-	      $(".password .fill").css({
-	        background: `linear-gradient(${pie1}deg, transparent 50%, ${pieColor} 50%), linear-gradient(90deg, ${lightGrey} 50%, transparent 50%)`
-	      });
-	      if (checkInterval === 250) {
-	        clearInterval(myInterval);
-	      }
-	      login.click(function() {
-	        $(".password .content").css("color", "#fff");
-	        return clearInterval(myInterval);
-	      });
-	      return checkInterval++;
+	    
+	    return myInterval = setInterval((
+	    		function() { 	
+	    			var backFill, color, pie1, pieColor, secureVal, value;
+	    				value = $(".password .content").val();
+	    			if (value.length > 0 && value.length < 4) {
+	    				color = red;
+	    				backFill = red;
+	    			} else if (value.length >= 5 && value.length < 7) {
+	    				color = yellow;
+	    				backFill = yellow;
+	    			} else if (value.length >= 8) {
+	    				color = emerald;
+	    				backFill = emerald;
+	    			}
+	    			
+	    			if (value.length > 0) {
+	    				$(".security").removeClass("hide");
+	    			} else {
+	    				$(".security").addClass("hide");
+	    			}
+	    			
+	    			secureVal = value.length * 9;
+	    			if (secureVal >= 100) {
+	    				secureVal = 100;
+	    			}
+	    			if (value.length <= 5) {
+	    				pie1 = (value.length * 36) + 90;
+	    				pieColor = lightGrey;
+	    				
+	    			} else if (value.length >= 5 && value.length <= 9) {
+	    				pieColor = color;
+	    				pie1 = (value.length * 36) - 90;
+	    				
+	    			} else {
+	    				secureVal = 90;
+	    				pie1 = 270;
+	    			}
+	    			
+	    			$(".secureValue").html(secureVal);
+	    			$(".password .content, .password .security-type").css("color", `${color}`);
+	    			$(".circle.background").css("background", `${backFill}`);
+	    			$(".password .fill").css({
+	    				background: `linear-gradient(${pie1}deg, transparent 50%, ${pieColor} 50%), linear-gradient(90deg, ${lightGrey} 50%, transparent 50%)`
+	    			});
+	    			
+	    			if (checkInterval === 250) {
+	    				clearInterval(myInterval);
+	    			}
+	    			login.click(function() {
+	    				$(".password .content").css("color", "#fff");
+	    				return clearInterval(myInterval);
+	    			});
+	    			return checkInterval++;
 	    }), 250);
 	  };
 
@@ -143,9 +164,11 @@
 
 	  
 	  signup.click(function() {
-	    if ($(".password").hasClass("ani-hide")) {
+		if ($(".password").hasClass("ani-hide")) {
 	      return toLogin();
-	    } else if (!signup.hasClass("selected")) {
+	    } 
+	    
+	    if (!signup.hasClass("selected")) {
 	      return toSignup();
 	    }
 	  });
@@ -158,10 +181,16 @@
 		  if(signup.hasClass('selected') && psw1.val() != psw2.val()){
 			  psw1.val('');
 			  psw2.val('');
-			  $(".security").addClass("hide")
+			  
+			  $(".se	curity").addClass("hide");
+			  
+			  validate.text('* password mismatch *');
+			  validate.removeClass("hide");
+			  validate.addClass("display");
 			  return;
 		  }
-		  
+		  validate.addClass("hide");
+		  validate.removeClass("display");
 	      $(".text-wrapper").removeClass("show");
 	      $(".load-gif").addClass("show");
 	      
@@ -169,7 +198,9 @@
 	        toLogin();
 	        submitForm();
 	        $(".text-wrapper").addClass("show");
+	        
 	        return $(".load-gif").removeClass("show");
+	        
 	      }), 1500);
 	      
 	  });
@@ -184,13 +215,14 @@
 	    if ($(".sign-up").hasClass("selected")) {
 	      return passwordSecure();
 	    }
+	    return;
 	  });
 	  
 
 	  $(".email .content").click(() => {
 	    return emailVerify();
 	  });
-	  
+	}
 
-	}).call(this);
+).call(this);
 
